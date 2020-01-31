90f188fafc2fc754c278922213c0c8b912d03941
Tyler Knapp <tgknapp11@gmail.com>
2020-01-31 13:07:31 -0500

    cleaned up backend files

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/tasks", { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
